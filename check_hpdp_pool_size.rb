08e118c7fec944806002363307256aa41d0ab848
Jean-Denis Gebhardt <jean-denis.gebhardt@noris.de>
2020-02-22 19:27:35 +0100

    Add perfstats to check_hpdp_pool_size

--- check_hpdp_pool_size.rb
+++ check_hpdp_pool_size.rb
@@ -15,13 +15,13 @@
 #
 # Copyright (c) 2016 Jean-Denis Gebhardt
 #
-
 require 'optparse'
 require 'pp'
 
 options    = {}
 list       = {}
-usage      = []
+usage      = { :out => [], :perf => [] }
+media_stat = []
 ex         = 3
 
 OptionParser.new do |opts|
@@ -59,10 +59,40 @@ def get_info(pool = "")
   end
 end
 
-def get_usage(pool = "", total = 0.00, used = 0.00, warn = 75, crit = 95)
-  up = (100.00 * used / total).round
+def get_media_info(pool = "")
+  if pool.empty?
+    puts "no poolname given!"
+    exit 3
+  else
+    media      = {}
+    id         = []
+    label      = []
+    status     = []
+    protection = []
 
-  return "Pool \"#{ pool }\": #{ total.round }GB Total; #{ used.round }GB (#{ up }%) Used | \'Pool Size #{ pool }\'=#{ used.round }GB;#{ (warn.to_f / 100) * total.round };#{ (crit.to_f / 100) * total.round };0;#{ total.round }"
+    `/opt/omni/bin/omnimm -list_pool #{ pool } -detail | awk '$0 ~ /Medium identifier|Medium label|Status|Protected/'`.split("\n").each do |info|
+      id         << info.match(/Medium identifier : (.+)\s+/)[1].squeeze(" ") if info.match(/Medium identifier/)
+      label      << info.match(/Medium label             : (.+)\s+$/)[1].squeeze(" ") if info.match(/Medium label/)
+      status     << info.match(/Status                   : (.+)\s+$/)[1].squeeze(" ") if info.match(/Status/)
+      protection << info.match(/Protected                : (.+)/)[1].squeeze(" ") if info.match(/Protected/)
+    end
+
+    label.each_with_index do |l, i|
+      media[l.strip] = {
+       :id         => id[i].strip,
+       :status     => status[i].strip,
+       :protection => protection[i].strip }
+    end
+  end
+  return media
+end
+
+def get_usage(pool = "", total = 1.00, used = 1.00, warn = 75, crit = 95)
+  up = total == 0 ? 0 : (100.00 * used / total).round
+  return {
+    :out  => "Pool \"#{ pool }\": #{ total.round }GB Total; #{ used.round }GB (#{ up }%) Used",
+    :perf => "\'Pool Size #{ pool }\'=#{ used.round }GB;#{ (warn.to_f / 100) * total.round };#{ (crit.to_f / 100) * total.round };0;#{ total.round }"
+  }
 end
 
 def set_group(filter = "", pools = {})
@@ -100,7 +130,19 @@ def set_ex(filter = "", pools = {}, warn = 75, crit = 90)
   end
   return ex
 end
-###############
+
+def set_media_ex(media = {})
+  ex = 3
+  return ex if media.empty?
+  case
+    when media[:status] == "Good" then ex = 0
+    when media[:status] == "Fair" then ex = 1
+    when media[:status] == "Poor" then ex = 2
+    when media[:protection] == "Permanent" then ex = 2
+    else ex = 3
+  end
+  return ex
+end
 
 p = `/opt/omni/bin/omnimm -show_pool | /bin/awk '$0 ~ /#{ options[:pool] }/ {print $2}'`.split
 p.each do |o|
@@ -121,9 +163,26 @@ list.each do |key,val|
   unless key.match(/freepool/)
     total = val[:freepool].nil? || val[:freepool].empty? ? val[:size] : val[:size] + list["#{ val[:freepool] }"][:size]
     used  = val[:used]
-    usage << get_usage(key, total, used, options[:warning], options[:critical])
+    tmp_usage ||= { :out => [], :perf => [] }
+    tmp_usage   = get_usage(key, total, used, options[:warning], options[:critical])
+    usage[:out] << tmp_usage[:out]
+    usage[:perf] << tmp_usage[:perf]
+
+    unless options[:filter].include?(key)
+      list[key][:media] = get_media_info(key)
+      list[key][:media].each do |key, val|
+        e  = set_media_ex(val)
+        media_stat << "Fair media found: #{ key }" if e == 1
+        media_stat << "Poor media or media with permanent protection found: #{ key }" if e == 2
+        ex = e if ex == 3
+        ex = e if (e == 1 || e == 2 ) && e > ex
+      end
+    end
   end
 end
-puts usage.join("\n")
+
+puts usage[:out].sort.join("\n")
+puts "| " + usage[:perf].sort.join(" ")
+puts media_stat.join("\n") unless media_stat.empty?
 
 exit ex
